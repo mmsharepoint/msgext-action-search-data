92c48bf59a237f2117a8879e18f9e49632fa39f4
mmsharepoint <markus.moeller.ed@outlook.de>
2024-01-08 12:57:02 +0100

    Fixed non-bound dataSrc, sectect

--- MsgextActionSrchData/Program.cs
+++ MsgextActionSrchData/Program.cs
@@ -36,6 +36,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    builder.Configuration["BotEndpoint"] = Environment.GetEnvironmentVariable("VS_TUNNEL_URL");
 }
 app.UseStaticFiles();
 
