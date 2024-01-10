c478b924c10b538153fe7765109e1e300fbb4038
mmsharepoint <markus.moeller.ed@outlook.de>
2024-01-10 09:20:31 +0100

    Simple submitTask added

---
+++ MsgextActionSrchData/wwwRoot/js/SbmData.js
@@ -0,0 +1,23 @@
+﻿console.log("Script loader before?");
+(function (SbmData) {
+  
+  SbmData.getContext = function () {
+    if (microsoftTeams) {
+      microsoftTeams.initialize();
+    }
+    console.log("Script loades");
+  }
+  SbmData.submitTask = function () {
+    if (microsoftTeams) {
+      microsoftTeams.dialog.submit(
+        {
+          Id: "1",
+          Name: "Produc1",
+          Orderable: true,
+          Orders: 18
+        }
+      )
+    };
+  }
+  
+}(window.SbmData = window.SbmData || {}));
