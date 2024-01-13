0ef031a8a8e021d3952afec45f1e2f25fea5c93b
mmsharepoint <markus.moeller.ed@outlook.de>
2024-01-13 11:11:24 +0100

    SubmitTask implemented

--- MsgextActionSrchData/Action/ActionApp.cs
+++ MsgextActionSrchData/Action/ActionApp.cs
@@ -13,29 +13,43 @@ namespace MsgextActionSrchData.Action;
 
 public class ActionApp : TeamsActivityHandler
 {
-    private readonly string _adaptiveCardFilePath = Path.Combine(".", "Resources", "helloWorldCard.json");
+    private readonly string _adaptiveBaseCardFilePath = Path.Combine(".", "Resources");
     // Action.
     protected override async Task<MessagingExtensionActionResponse> OnTeamsMessagingExtensionSubmitActionAsync(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action, CancellationToken cancellationToken)
     {
         if (action.CommandId == "selectItem")
         {
+            string _adaptiveCardFilePath = Path.Combine(_adaptiveBaseCardFilePath, "ProposedOrder.json");
             var actionData = ((JObject)action.Data).ToObject<Product>();
             string prodId = actionData.Id ?? "";
-            string prodName = actionData.Id ?? "";
+            string prodName = actionData.Name ?? "";
             string prodOrders = actionData.Orders.ToString() ?? "";
+            string prodOrderable = actionData.Orderable.ToString() ?? "false";
+
+            var templateJson = await System.IO.File.ReadAllTextAsync(_adaptiveCardFilePath, cancellationToken);
+            var template = new AdaptiveCards.Templating.AdaptiveCardTemplate(templateJson);
+            var adaptiveCardJson = template.Expand(new { ID = prodId, Name = prodName, Orders = prodOrders });
+            var adaptiveCard = AdaptiveCard.FromJson(adaptiveCardJson).Card;
+            var attachments = new MessagingExtensionAttachment()
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = adaptiveCard
+            };
 
             return new MessagingExtensionActionResponse
             {
                 ComposeExtension = new MessagingExtensionResult
                 {
-                    Type = "message",
-                    Text = prodId + " " + prodName + " " + prodOrders
+                    Type = "result",
+                    AttachmentLayout = "list",
+                    Attachments = new[] { attachments }
                 }
             };
         }
         else
         {
             // The user has chosen to create a card by choosing the 'Create Card' context menu command.
+            string _adaptiveCardFilePath = Path.Combine(_adaptiveBaseCardFilePath, "helloWorldCard.json");
             var actionData = ((JObject)action.Data).ToObject<CardResponse>();
             var templateJson = await System.IO.File.ReadAllTextAsync(_adaptiveCardFilePath, cancellationToken);
             var template = new AdaptiveCards.Templating.AdaptiveCardTemplate(templateJson);
